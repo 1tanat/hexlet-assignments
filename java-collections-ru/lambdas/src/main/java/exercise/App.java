b89765ba5bae3932f246c1791d91623e3ba34842
@hexlet/cli <support@hexlet.io>
2023-11-21 19:07:43 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -3,5 +3,21 @@ package exercise;
 import java.util.Arrays;
 
 // BEGIN
+class App {
+    public static String[][] enlargeArrayImage(String[][] image) {
+        String[][] image1 = Arrays.stream(image)
+                .map(App::duplicate)
+                .toArray(String[][]::new);
 
+        return Arrays.stream(image1)
+                .flatMap(item -> Arrays.stream(new String[][] {item, item}))
+                .toArray(String[][]::new);
+    }
+
+    public static String[] duplicate(String[] partImage) {
+        return Arrays.stream(partImage)
+                .flatMap(item -> Arrays.stream(new String[] {item, item}))
+                .toArray(String[]::new);
+    }
+}
 // END
--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -4,5 +4,55 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 // BEGIN
+class AppTest {
+    @Test
+    void testFromExample() {
+        String[][] image = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
 
+        String[][] actual = App.enlargeArrayImage(image);
+        String[][] expected = {
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+        };
+        assertThat(actual).isDeepEqualTo(expected);
+    }
+
+    @Test
+    void testFromSolved() {
+        String[][] image3 = {
+                {"@", "@"},
+                {"@", "|"},
+                {"—", "|"},
+                {"@", "|"},
+                {"@", "@"},
+        };
+
+        String[][] enlargedImage3 = {
+                {"@", "@", "@", "@"},
+                {"@", "@", "@", "@"},
+                {"@", "@", "|", "|"},
+                {"@", "@", "|", "|"},
+                {"—", "—", "|", "|"},
+                {"—", "—", "|", "|"},
+                {"@", "@", "|", "|"},
+                {"@", "@", "|", "|"},
+                {"@", "@", "@", "@"},
+                {"@", "@", "@", "@"},
+        };
+
+        String[][] actual3 = App.enlargeArrayImage(image3);
+        assertThat(actual3).isDeepEqualTo(enlargedImage3);
+    }
+}
 // END
