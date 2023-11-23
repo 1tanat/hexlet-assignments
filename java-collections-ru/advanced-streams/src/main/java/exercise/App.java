72e5966fe8e379d812337c4cc79420020406aafb
@hexlet/cli <support@hexlet.io>
2023-11-23 17:04:17 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -8,29 +8,17 @@ import java.util.stream.Stream;
 // BEGIN
 class App {
     public static String getForwardedVariables(String text) {
-        //String[] strings = text.split("\\R");
-        String[] asf = Stream.of(text.split("\\R"))
+        String[] strings = text.split("\\R");
+        return Arrays.stream(strings)
                 .filter(x -> x.startsWith("environment"))
                 .filter(x -> x.contains("X_FORWARDED_"))
                 .map(x -> x.replaceAll("environment=", ""))
                 .map(x -> x.replaceAll("\"", ""))
-                .toArray(String[]::new);
-
-        String asf2 = Arrays.toString(asf);
-        asf2 = asf2.replace("[", "").replace("]", "");
-
-        String[] asf1 = Stream.of(asf2.split(","))
-                .map(x -> x.replaceAll(" ", ""))
+                .map(x -> x.split(","))
+                .flatMap(Arrays::stream)
                 .filter(x -> x.startsWith("X_FORWARDED_"))
                 .map(x -> x.replaceAll("X_FORWARDED_", ""))
-                .peek(System.out::println)
-                .toArray(String[]::new);
-
-        String result = Arrays.toString(asf1);
-        result = result.replace("[", "").replace("]", "").replace(" ", "");
-
-        return result;
-
+                .collect(Collectors.joining(","));
     }
 }
 //END
